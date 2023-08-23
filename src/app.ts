95c57bed856ac4ef8c08c47ea46b8bd1233b84b7
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2023-08-23 16:51:03 +0530

    feat: add cheqd module
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- src/app.ts
+++ src/app.ts
@@ -1,97 +1,111 @@
-import console from 'console'
 import { issuer } from './issuer'
 import { holder } from './holder'
-import {
-  DidDocument,
-  DidDocumentBuilder,
-  JsonTransformer,
-  KeyType,
-  OutOfBandState,
-  TypedArrayEncoder,
-  getEd25519VerificationKey2018,
-} from '@aries-framework/core'
+import { KeyType, TypedArrayEncoder, V2CredentialPreview } from '@aries-framework/core'
+import { uuid } from '@aries-framework/core/build/utils/uuid'
 
 async function app() {
   await issuer.initialize()
   await holder.initialize()
+  await holder.modules.anoncreds.createLinkSecret()
   issuer.config.logger.info('Agents initialized!')
 
-  const domain = 'sairanjitaw.github.io'
-  const did = `did:web:${domain}`
-  const keyId = `${did}#key-1`
+  let anoncredsIssuerId = ''
 
-  const key = await issuer.wallet.createKey({
-    keyType: KeyType.Ed25519,
-    privateKey: TypedArrayEncoder.fromString('afjdemoverysercure00000000000000'),
+  // NOTE: we assume the did is already registered on the ledger, we just store the private key in the wallet
+  // and store the existing did in the wallet
+  const privateKey = TypedArrayEncoder.fromString('afjdemoverysercure00000000000003')
+
+  // Create key
+  await issuer.wallet
+    .createKey({
+      keyType: KeyType.Ed25519,
+      privateKey,
+    })
+    .then((data) => console.log('[createKey] data', JSON.stringify(data)))
+    .catch((error) => console.log('[createKey] error', error))
+
+  // Publish did
+  const didResult = await issuer.dids.create({
+    method: 'cheqd',
+    secret: {
+      verificationMethod: {
+        id: 'key-1',
+        type: 'JsonWebKey2020',
+      },
+    },
+    options: {
+      network: 'testnet',
+    },
   })
 
-  const didDocument = new DidDocumentBuilder(did)
-    .addContext('https://w3id.org/security/suites/ed25519-2018/v1')
-    .addVerificationMethod(getEd25519VerificationKey2018({ key, id: keyId, controller: did }))
-    .addAuthentication(keyId)
-    .build()
+  if (didResult.didState.did) {
+    console.log(`DID: ${didResult.didState.did}`)
+    anoncredsIssuerId = didResult.didState.did
+  } else {
+    console.log('Failed to register DID, try again')
+  }
 
-  await issuer.dids.import({
-    did,
-    overwrite: true,
-    didDocument,
+  // Register Schema
+  const schemaTemplate = {
+    name: 'Faber College' + uuid(),
+    version: '1.0.0',
+    attrNames: ['name', 'degree', 'age'],
+    issuerId: anoncredsIssuerId,
+  }
+  console.log(schemaTemplate.name, schemaTemplate.version, schemaTemplate.attrNames)
+  console.log('Registering schema...')
+
+  const { schemaState } = await issuer.modules.anoncreds.registerSchema({
+    schema: schemaTemplate,
+    options: {},
   })
 
-  // const issuerId = `did:web:sairanjitaw.github.io`
-  // const privateKey = '73f80dcde8be30e538ea8bafeb4701d098c5ea72720a51dc750527f4b78f01b2'
-  // await issuer.dids.import({
-  //   did: issuerId,
-  //   overwrite: true,
-  //   privateKeys: [
-  //     {
-  //       keyType: KeyType.Ed25519,
-  //       privateKey: TypedArrayEncoder.fromHex(privateKey),
-  //     },
-  //   ],
-  // })
+  if (!schemaState.schemaId) {
+    throw new Error(`Invalid schema ${schemaState.schemaId}`)
+  }
 
-  const dids = await issuer.dids.getCreatedDids({ did })
-  console.log('dids', JSON.stringify(dids))
+  // Create Cred Def
+  const { credentialDefinitionState } = await issuer.modules.anoncreds.registerCredentialDefinition({
+    credentialDefinition: {
+      schemaId: schemaState.schemaId,
+      issuerId: anoncredsIssuerId,
+      tag: 'latest',
+    },
+    options: {},
+  })
+
+  if (!credentialDefinitionState.credentialDefinitionId) {
+    throw new Error(`Invalid credentialDefinition ${credentialDefinitionState.credentialDefinitionId}`)
+  }
+
+  // Create credential preview
+  const credentialPreview = V2CredentialPreview.fromRecord({
+    name: 'Alice Smith',
+    degree: 'Computer Science',
+    age: '22',
+  })
 
   // Create out of band invitation
 
-  const inv = await holder.oob.createLegacyInvitation({
+  const inv = await holder.oob.createInvitation({
     autoAcceptConnection: true,
   })
-  const { connectionRecord } = await issuer.oob.receiveInvitation(inv.invitation)
+  const { connectionRecord } = await issuer.oob.receiveInvitation(inv.outOfBandInvitation)
   if (!connectionRecord) {
     throw new Error('Connection not found')
   }
 
   await issuer.connections.returnWhenIsConnected(connectionRecord.id)
 
-  const usingDid = dids.find((record) => record.did.includes('did:web'))?.did
-
-  console.log('\n\n\nusingDid***', usingDid)
-
   const credRecord = await issuer.credentials.offerCredential({
     connectionId: connectionRecord.id,
+    protocolVersion: 'v2',
     credentialFormats: {
-      jsonld: {
-        credential: {
-          '@context': ['https://www.w3.org/2018/credentials/v1', 'https://www.w3.org/2018/credentials/examples/v1'],
-          type: ['VerifiableCredential', 'UniversityDegreeCredential'],
-          issuer: { id: usingDid! },
-          issuanceDate: new Date().toISOString(),
-          credentialSubject: {
-            degree: {
-              type: 'BachelorDegree',
-              name: 'Bachelor of Science and Arts',
-            },
-          },
-        },
-        options: {
-          proofType: 'Ed25519Signature2018',
-          proofPurpose: 'assertionMethod',
-        },
+      anoncreds: {
+        attributes: credentialPreview.attributes,
+        credentialDefinitionId: credentialDefinitionState.credentialDefinitionId,
       },
     },
-    protocolVersion: 'v2',
   })
 }
 
